68c2e0eaa24c537a28df5f76faf6d86c8ef04930
Your Name <your@email.address>
2016-02-02 10:15:12 -0500

    Finish exercise 1.1

--- src/answers.scm
+++ src/answers.scm
@@ -5,3 +5,74 @@
 
 (assert "Exercise 1.1.2"
   (= (+ 5 3 4) 12))
+
+(assert "Exercise 1.1.3"
+  (= (- 9 1) 8))
+
+(assert "Exercise 1.1.4"
+  (= (/ 6 2) 3))
+
+(assert "Exercise 1.1.5"
+  (=
+    (+ (* 2 4) (- 4 6))
+    6))
+
+(assert "Exercise 1.1.6"
+  (and
+    (define a 3)
+    (= a 3)))
+
+(assert "Exercise 1.1.7"
+  (and
+    (define b (+ a 1))
+    (= b 4)))
+
+(assert "Exercise 1.1.8"
+  (=
+    (+ a b (* a b))
+    19))
+
+(assert "Exercise 1.1.9"
+  (equal?
+    (= a b)
+    #f))
+
+(assert "Exercise 1.1.10"
+  (=
+    (if
+      (and
+        (> b a)
+        (< b (* a b)))
+      b a)
+    b))
+
+(assert "Exercise 1.1.11"
+  (=
+    (cond
+      ((= a 4) 6)
+      ((= b 4) (+ 6 7 a))
+      (else 25))
+    16
+  )
+)
+
+(assert "Exercise 1.1.12"
+  (=
+    (+ 2 (if (> b a) b a))
+    6
+  )
+)
+
+(assert "Exercise 1.1.13"
+  (=
+    (*
+      (cond
+        ((> a b) a)
+        ((< a b) b)
+        (else -1)
+      )
+      (+ a 1)
+    )
+    16
+  )
+)
