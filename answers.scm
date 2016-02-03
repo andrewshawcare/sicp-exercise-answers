f4409060cdccbcf2f43b0cb10faa30575a4cfdc7
Your Name <your@email.address>
2016-02-02 21:33:13 -0500

    Add answer for exercise 1.7

--- answers.scm
+++ answers.scm
@@ -116,3 +116,31 @@
     (= (new-if (= 1 1) x (set! x 1)) 1)
   )
 )
+
+(define (good-enough? guess x)
+  (< (abs (- 1 (/ (square guess) x))) 0.001))
+
+(define (average x y)
+  (/ (+ x y) 2))
+
+(define (improve guess x)
+  (average guess (/ x guess)))
+
+(define (sqrt-iter guess x)
+  (if (good-enough? guess x)
+    guess
+    (sqrt-iter (improve guess x) x)))
+
+(define (sqrt x)
+  (sqrt-iter 1.0 x))
+
+(define (acceptable-square? x tolerance)
+  (< (abs (- 1 (/ (square (sqrt x)) x))) tolerance))
+
+(assert "Exercise 1.7"
+  (let ((tolerance 0.001))
+    (and
+      (acceptable-square? 0.00000000001 tolerance)
+      (acceptable-square? 1000000000000 tolerance))
+  )
+)
